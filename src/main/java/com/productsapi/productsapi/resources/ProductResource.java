d539d3c1618deb3c71ef968ed6a6dc6e69c9b8fd
Felipe Guimarães <felipeguimaraes2406@gmail.com>
2021-04-23 01:13:10 -0300

    create method for delete product

--- src/main/java/com/productsapi/productsapi/resources/ProductResource.java
+++ src/main/java/com/productsapi/productsapi/resources/ProductResource.java
@@ -1,9 +1,11 @@
 package com.productsapi.productsapi.resources;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -33,9 +35,12 @@ public class ProductResource {
 	
 	@PostMapping("/products")
 	public Product saveProduct(@RequestBody Product product) {
-		
 		return productRepository.save(product);
-		
+	}
+	
+	@DeleteMapping("/products")
+	public void deleteProduct(@RequestBody Product product) {
+		productRepository.delete(product);
 	}
 	
 }
