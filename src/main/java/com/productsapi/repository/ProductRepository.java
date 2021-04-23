683a826fe4099c065d4aae81070639da5e97d9a6
Felipe Guimarães <felipeguimaraes2406@gmail.com>
2021-04-23 00:42:04 -0300

    add product repository and docker file

---
+++ src/main/java/com/productsapi/repository/ProductRepository.java
@@ -0,0 +1,9 @@
+package com.productsapi.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.productsapi.models.Product;
+
+public interface ProductRepository extends JpaRepository<Product, Long>{
+
+}
