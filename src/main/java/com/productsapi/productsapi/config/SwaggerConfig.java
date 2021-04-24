d205c64ba3669364d1e1780ecab3fc7232e655ed
Felipe Guimarães <felipeguimaraes2406@gmail.com>
2021-04-23 21:08:49 -0300

    update swagger and add path route to /api

--- src/main/java/com/productsapi/productsapi/config/SwaggerConfig.java
+++ src/main/java/com/productsapi/productsapi/config/SwaggerConfig.java
@@ -8,24 +8,31 @@ import springfox.documentation.service.Contact;
 import springfox.documentation.builders.RequestHandlerSelectors;
 import springfox.documentation.service.VendorExtension;
 import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spring.web.paths.RelativePathProvider;
 import springfox.documentation.spring.web.plugins.Docket;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
 import static springfox.documentation.builders.PathSelectors.regex;
 
 import java.util.ArrayList;
 
+import javax.servlet.ServletContext;
+
+import static springfox.documentation.builders.PathSelectors.regex;
+
 @Configuration
 @EnableSwagger2
 public class SwaggerConfig {
 
 	@Bean
-	public Docket productApi() {
-		return new Docket(DocumentationType.SWAGGER_2)
-				.select()
-				.apis(RequestHandlerSelectors.basePackage("com.productsapi.productsapi"))
-				.paths(regex("api.*"))
-				.build()
-				.apiInfo(metaInfo());
+	public Docket productApi(ServletContext servletContext) {
+	    return new Docket(DocumentationType.SWAGGER_2)
+    		.pathProvider(new RelativePathProvider(servletContext) {
+    			@Override
+    			public String getApplicationBasePath() {
+    				return "/api" + super.getApplicationBasePath();
+    			}})
+    		.host("proxyURL")
+    		.apiInfo(metaInfo());
 	}
 	
 	private ApiInfo metaInfo() {
